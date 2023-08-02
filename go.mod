539fb9fe73e2ddd1bd642644d676e6b4b40899cd
bsyrlhabibi <bsyrlhabibi@gmail.com>
2023-08-02 17:15:11 +0700

    mencoba major upgrade

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/bsyrlhabibi/go-belajar-module
+module github.com/bsyrlhabibi/go-belajar-module/v2
 
 go 1.20
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_belajar_module
 
-func SayHello() string {
-	return "Hello World"
+func SayHello(name string) string {
+	return "Hello" + name
 }
